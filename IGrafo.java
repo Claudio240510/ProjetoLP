5c7bd9e77c823604a9f09e815707665a2422e852
170100250 ESG <63797812+Claudio240510@users.noreply.github.com>
2020-05-28 11:46:47 +0100

    Add files via upload
    Interface com os métodos a implementar na classe Grafo

---
+++ IGrafo.java
@@ -0,0 +1,13 @@
+package TrabalhoLP;
+
+public interface IGrafo {
+
+	public int addCidade(String nome);
+
+	public void addLigacao(Cidade origem, Cidade destino, double horas);
+
+	public void imprimeLigacoes();
+
+	public void encontraMenorCaminho(Cidade origem, Cidade destino);
+}
+}
