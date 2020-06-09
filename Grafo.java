b651788c718e90518f61d0e3318d651622425d3b
170100250 ESG <63797812+Claudio240510@users.noreply.github.com>
2020-06-09 22:32:30 +0100

    Update Grafo.java
    Cidades por visitar e limpa Cidades visitadas

--- Grafo.java
+++ Grafo.java
@@ -5,27 +5,27 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Grafo {
-
-	private Set<Cidade> cidades;
-	private boolean grafoDirecionado;
+	public class Grafo {
+		private Set<Cidade> cidades;
+		private boolean grafoDirecionado;
 
 	// Construtores
-
 	public Grafo(boolean grafoDirecionado) {
 		this.grafoDirecionado = grafoDirecionado;
 		cidades = new HashSet<>();
+	}
+	
+	public Grafo() {
+		grafoDirecionado=true;
 	}
 
 	// Getters e setters
 
 	public void addCidade(Cidade n) {
-
 		cidades.addAll(Arrays.asList(n));
 	}
 
 	public void addLigacao(Cidade origem, Cidade destino, double horas) {
-
 		cidades.add(origem);
 		cidades.add(destino);
 
@@ -37,7 +37,6 @@ public class Grafo {
 	}
 
 	private void adicionaLigacao(Cidade a, Cidade b, double horas) {
-
 		for (Ligacao ligacao : a.ligacoes) {
 			if (ligacao.getOrigem() == a && ligacao.getDestino() == b) {
 
@@ -75,6 +74,31 @@ public class Grafo {
 			}
 		}
 		return false;
+	}
+	
+	public void limpaCidadesVisitadas() {
+	for (Cidade cidade : cidades) {
+		cidade.naoVisitado();
+		}
+	}
+	
+	private Cidade cidadeVizinhaPorVisitar(HashMap<Cidade, Double> caminhoMaisCurto) {
+		double menorDistancia = 1000000000000000.0;
+		Cidade cidadeMaisProxima = null;
+		for (Cidade cidade : cidades) {
+			if (cidade.isFoiVisitado())
+				continue;
+			
+			double distanciaAtual = caminhoMaisCurto.get(cidade);
+			if (distanciaAtual == 1000000000000000.0)
+				continue;
+
+			if (distanciaAtual < menorDistancia) {
+				menorDistancia = distanciaAtual;
+				cidadeMaisProxima = cidade;
+			}
+		}
+		return cidadeMaisProxima;
 	}
 
 }
