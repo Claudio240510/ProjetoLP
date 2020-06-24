fe67ced3d3615d788d0c6a9f94ac9aa5b7668a1c
170100250 ESG <63797812+Claudio240510@users.noreply.github.com>
2020-06-24 17:51:25 +0100

    Update Grafo.java

--- Grafo.java
+++ Grafo.java
@@ -1,43 +1,7 @@
 import java.util.*;
 
-public class Grafo implements IGrafo {
+public class Grafo implements IGrafo{
 
-	private Set<Cidade> cidades;
-	private PriorityQueue<Cidade> queueCidade = new PriorityQueue<>();
-
-	public Grafo() {
-		cidades = new HashSet<>();
-	}
-	@Override
-	public void addCidade(Cidade n) {
-		cidades.add(n);
-	}
-	
-	@Override
-	public void removerCidade(Cidade n) {
-		cidades.remove(n);
-	}
-
-	@Override
-	public void encontraMenorCaminho(Cidade origem){
-		origem.setDistanciaMinima(0.0);
-		queueCidade.add(origem);
-		while (!queueCidade.isEmpty()) {
-			Cidade c = queueCidade.poll();
-			for (Ligacao l : c.getLigacoes()) {
-				Cidade destino = l.getDestino();
-				double peso = l.getHoras();
-				double distanciaAtravesC = peso + c.getDistanciaMinima();
-				if(distanciaAtravesC < destino.getDistanciaMinima()) {
-					destino.setDistanciaMinima(distanciaAtravesC);
-					destino.setCidadeAnterior(c);
-					queueCidade.remove(destino);
-					queueCidade.add(destino);
-				}
-			}
-		}
-	}
-	
 	private Set<Cidade> cidades;
 	private PriorityQueue<Cidade> queueCidade = new PriorityQueue<>();
 
@@ -94,18 +58,6 @@ public class Grafo implements IGrafo {
 			}
 		}
 	}
-	
-	@Override
-	public List<Cidade> criarCaminho(Cidade destino){
-		List<Cidade> caminho = new ArrayList<Cidade>();
-		for (Cidade c = destino; c!=null; c=c.getCidadeAnterior()) {
-			caminho.add(c);
-		}
-		Collections.reverse(caminho);
-		return caminho;
-	}
-	
-}
 	
 	@Override
 	public List<Cidade> criarCaminho(Cidade destino){
